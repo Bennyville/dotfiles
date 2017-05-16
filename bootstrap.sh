038b5bf7c7aa0775f9d270c229b433cb41dc5ee3
Benny Peckruhn <bpeckruhn@bpeckruhn.local>
2017-05-16 09:09:00 +0200

    use alias for airport cli

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # Create a symlink for airport cli
-sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+
+
+# 
+
+
