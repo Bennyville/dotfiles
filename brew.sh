c9b5393769d8bd0ad7c40f34ce4c1ed9089080f7
Benny Peckruhn <bennypeck@gmail.com>
2017-05-18 00:06:46 +0200

    add brew install script

---
+++ brew.sh
@@ -0,0 +1,33 @@
+brew upgrade
+brew update
+
+# CLI Tools
+brew install htop
+brew install mercurial
+brew install node
+brew install php70
+brew install python
+brew install shellcheck
+brew install smartmontools
+brew install speedtest_cli
+brew install task
+brew install thefuck
+brew install tree
+brew install wget
+brew install woof
+brew install zsh
+
+# Application
+brew cask install 1password
+brew cask install adobe-creative-cloud
+brew cask install appcleaner
+brew cask install google-chrome-canary
+brew cask install iterm2-nightly
+brew cask install sequel-pro
+brew cask install sketch
+brew cask install skype
+brew cask install sublime-text-dev
+brew cask install virtualbox
+brew cask install vlc
+
+brew cleanup
