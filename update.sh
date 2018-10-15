74edcb65e9d8686a3a4ec9b640e1950ff09a4391
Benny Peckruhn <bennypeck@gmail.com>
2018-10-15 09:33:36 +0200

    add output

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env zsh
 
+#
 curl -I -L "http://github.com" --silent > /dev/null
 [[ $? -eq 7 ]] && echo "Can't reach github.com. Proxy needed?" && exit
 
+echo '### Pull latest changes ###'
 git pull origin master
 
+echo '\n### Sync dotfiles ###'
 # Sync dotfiles
 rsync --exclude ".git/" \
 	--exclude ".DS_Store" \
@@ -12,6 +15,7 @@ rsync --exclude ".git/" \
 	--exclude "update.sh" \
 	-avh --no-perms . ~;
 
+echo '\n### Reload ZSH config ###'
 source ~/.zshrc
 
 # Brew
@@ -21,6 +25,7 @@ function update_homebrew() {
 	brew cleanup
 }
 
+echo '\n### Update homebrew ###'
 read "confirm?Update homebrew? (y/n)"
 if [[ "$confirm" =~ ^[Yy]$ ]]
 then
