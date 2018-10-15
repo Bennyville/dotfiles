710cf8db032124082f69c95a8b132a555927f371
Benny Peckruhn <bennypeck@gmail.com>
2018-10-15 10:36:31 +0200

    update brew install script

--- brew.sh
+++ brew.sh
@@ -6,37 +6,64 @@ brew update
 # CLI Tools
 brew install coreutils
 brew install ack
+brew install ansiweather
+brew install bash
+brew install curl
+brew install elm
+brew install ffmpeg
+brew install gawk
 brew install git
+brew install highlight
 brew install htop
 brew install mercurial
+brew install nmap
 brew install node
 brew install php70
+brew install pv
 brew install python
 brew install python3
 brew install rename
 brew install shellcheck
 brew install smartmontools
 brew install speedtest_cli
+brew install svgo
 brew install task
 brew install thefuck
+brew install tidy-html5
 brew install tldr
+brew install tmux
+brew install translate-shell
 brew install tree
 brew install unrar
+brew install vagrant-completion
+brew install watch
 brew install wget
 brew install woof
+brew install yarn
 brew install zsh
+brew install zsh-completions
 
 # Application
 brew cask install 1password
-brew cask install adobe-creative-cloud
+brew cask install 1password-cli
+brew cask install alfred
 brew cask install appcleaner
+brew cask install firefox
+brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install iterm2-nightly
+brew cask install java
+brew cask install phpstorm
 brew cask install sequel-pro
 brew cask install sketch
+brew cask install sourcetree
 brew cask install skype
 brew cask install sublime-text-dev
+brew cask install tortoisehg
+brew cask install transmit
+brew cask install vagrant
 brew cask install virtualbox
+brew cask install visual-studio-code
 brew cask install vlc
 
 brew cleanup
