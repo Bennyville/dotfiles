40648cfd47b011906c3359d879517025634ea7da
Benny Peckruhn <benny@bennyville.de>
2023-03-08 16:01:42 +0100

    update brew install script

--- brew.sh
+++ brew.sh
@@ -4,62 +4,39 @@ brew upgrade
 brew update
 
 # CLI Tools
-brew install coreutils
 brew install ack
-brew install ansiweather
 brew install bash
 brew install curl
 brew install duf
-brew install elm
 brew install fd
 brew install ffmpeg
 brew install fzf
 brew install gawk
 brew install git
-brew install highlight
-brew install htop
 brew install mercurial
-brew install mozjpeg
-brew install nmap
 brew install node
 brew install nvim
-brew install php70
-brew install pv
 brew install python
 brew install python3
-brew install rename
-brew install shellcheck
-brew install smartmontools
-brew install speedtest_cli
-brew install svgo
-brew install task
-brew install the_silver_searcher
-brew install thefuck
-brew install tidy-html5
+brew install koekeishiya/formulae/skhd
 brew install tldr
 brew install tmux
-brew install translate-shell
-brew install tree
-brew install unrar
-brew install vagrant-completion
-brew install watch
 brew install wget
-brew install woof
-brew install yarn
 brew install zsh
 brew install zsh-completions
+brew install koekeishiya/formulae/yabai
 
 # Application
 brew install 1password
-brew install 1password-cli
 brew install alfred
 brew install appcleaner
 brew install docker
+brew install figma
 brew install firefox
 brew install google-chrome
-brew install java
 brew install monitorcontrol
 brew install phpstorm
-brew install vlc
+brew install slack
+brew install spotify
 
 brew cleanup
