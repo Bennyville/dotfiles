c8c78ac12e5b0639b6b4c6d3e93c30391889b155
Benny Peckruhn <benny@bennyville.de>
2023-04-03 22:59:54 +0200

    add dotfiles folder

--- scripts/tmux-session.sh
+++ scripts/tmux-session.sh
@@ -5,7 +5,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Code/lab ~/Code/misc ~/Code/work ~/Code/private -mindepth 1 -maxdepth 1 -type d | fzf)
+	selected=$((find ~/Code/lab ~/Code/misc ~/Code/work ~/Code/private -mindepth 1 -maxdepth 1 -type d && echo ~/.dotfiles) | fzf)
 fi
 
 if [[ -z $selected ]]; then
