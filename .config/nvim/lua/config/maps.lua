161f57702c32e36bef8e645892271aac51da5c0e
Benny Peckruhn <benny@bennyville.de>
2023-04-05 12:03:21 +0200

    add mapping to yank to system clipboard

--- .config/nvim/lua/config/maps.lua
+++ .config/nvim/lua/config/maps.lua
@@ -16,3 +16,5 @@ vim.keymap.set('n', '<Leader>ag', ':Ag <C-R><C-W><CR>')
 vim.keymap.set('', '<leader>dd', ':Explore %:p:h<CR>')
 vim.keymap.set('', '<leader>da', ':Explore<CR>')
 
+vim.keymap.set({'v', 'x', 'n'}, '<leader>y', '"+y')
+
