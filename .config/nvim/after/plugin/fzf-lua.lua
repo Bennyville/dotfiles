a38e3aa56b9e01a7a92ab81478f01fd5e377ba4a
Benny Peckruhn <benny@bennyville.de>
2023-10-06 14:11:10 +0200

    add mappings to search for contents

--- .config/nvim/after/plugin/fzf-lua.lua
+++ .config/nvim/after/plugin/fzf-lua.lua
@@ -1,4 +1,6 @@
 vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'fzf-lua\').live_grep()<cr>', {})
+vim.keymap.set('n', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_cword()<cr>', {})
+vim.keymap.set('v', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_visual()<cr>', {})
 vim.keymap.set('n', '<leader>ff', '<cmd>lua require(\'fzf-lua\').files()<cr>', {})
 vim.keymap.set('n', '<leader>fd', '<cmd>lua require(\'fzf-lua\').buffers()<cr>', {})
 vim.keymap.set('n', '<leader>fs', '<cmd>lua require(\'fzf-lua\').git_files()<cr>', {})
