15b5c671a4f7c9ffdf85b8a51ae7e299f8613145
Benny Peckruhn <benny@bennyville.de>
2025-01-28 17:59:31 +0100

    add filetype plugin for c

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,4 @@
+vim.bo.tabstop = 8 -- size of a hard tabstop (ts).
+vim.bo.shiftwidth = 8 -- size of an indentation (sw).
+vim.bo.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.bo.softtabstop = 8 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
