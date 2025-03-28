a173faae7d8400e7200caf0808c619d6dec2fcbe
Benny Peckruhn <bpeckruhn@bpeckruhn-M1.local>
2025-03-28 07:19:13 +0100

    add keymaps for trouble

--- .config/nvim/lua/plugins/config/trouble.lua
+++ .config/nvim/lua/plugins/config/trouble.lua
@@ -1,1 +1,5 @@
 require('trouble').setup {}
+
+vim.keymap.set('n', '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>')
+vim.keymap.set('n', ']t', '<cmd>Trouble diagnostics next<cr>')
+vim.keymap.set('n', '[t', '<cmd>Trouble diagnostics prev<cr>')
