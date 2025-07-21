d3881be7320900af5e2527ec7e79cf244e7bf0af
Benny Peckruhn <bpeckruhn@bpeckruhn-M1.local>
2025-07-21 11:59:05 +0200

    add keybind to add selection to quickfix list

--- .config/nvim/lua/plugins/config/fzf-lua.lua
+++ .config/nvim/lua/plugins/config/fzf-lua.lua
@@ -1,3 +1,11 @@
+require("fzf-lua").setup({
+    keymap = {
+        fzf = {
+            ["ctrl-q"] = "select-all+accept",
+        },
+    },
+})
+
 vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'fzf-lua\').live_grep()<cr>', {})
 vim.keymap.set('n', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_cword()<cr>', {})
 vim.keymap.set('v', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_visual()<cr>', {})
