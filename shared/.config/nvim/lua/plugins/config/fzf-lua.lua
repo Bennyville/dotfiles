3013fdb3593da6ddeff63ed6c808256382bf3883
Benny <benny@bennyville.de>
2026-04-14 08:09:34 +0200

    add fff.nvim

--- shared/.config/nvim/lua/plugins/config/fzf-lua.lua
+++ shared/.config/nvim/lua/plugins/config/fzf-lua.lua
@@ -6,10 +6,10 @@ require("fzf-lua").setup({
     },
 })
 
-vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'fzf-lua\').live_grep({resume=true})<cr>', {})
-vim.keymap.set('n', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_cword()<cr>', {})
+-- vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'fzf-lua\').live_grep({resume=true})<cr>', {})
+-- vim.keymap.set('n', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_cword()<cr>', {})
 vim.keymap.set('v', '<leader>fG', '<cmd>lua require(\'fzf-lua\').grep_visual()<cr>', {})
-vim.keymap.set('n', '<leader>ff', '<cmd>lua require(\'fzf-lua\').files()<cr>', {})
+-- vim.keymap.set('n', '<leader>ff', '<cmd>lua require(\'fzf-lua\').files()<cr>', {})
 vim.keymap.set('n', '<leader>fd', '<cmd>lua require(\'fzf-lua\').buffers()<cr>', {})
 vim.keymap.set('n', '<leader>fs', '<cmd>lua require(\'fzf-lua\').lsp_document_symbols()<cr>', {})
 vim.keymap.set('n', '<leader>fa', '<cmd>lua require(\'fzf-lua\').builtin()<cr>', {})
