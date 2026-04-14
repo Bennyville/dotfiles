455680adcd6db7e7cebb5bced8505633d7a0f67e
Benny <benny@bennyville.de>
2026-04-14 08:09:47 +0200

    fix treesitter for nvim 0.12

--- shared/.config/nvim/lua/plugins/init.lua
+++ shared/.config/nvim/lua/plugins/init.lua
@@ -50,6 +50,16 @@ return {
 		branch = 'main',
 		config = function()
 			require('plugins.config.treesitter')
+		end,
+		init = function()
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function()
+					-- Enable treesitter highlighting and disable regex syntax
+					pcall(vim.treesitter.start)
+					-- Enable treesitter-based indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
 		end
 	},
 	{ 'nvim-treesitter/nvim-treesitter-context' },
